60871184305f4c47361bbe1fc20452aa348705ee
gairui <gairui@mogujie.com>
2018-03-12 16:00:54 +0800

    update pbxproj

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f685e74"
-#define MARS_PATH "master"
+#define MARS_REVISION "26d973a"
+#define MARS_PATH "feature/mwcs_iOS_version_of_mars"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2017-10-12 18:37:49"
-#define MARS_TAG "mars_xlog_sdk/mars_libs"
+#define MARS_BUILD_TIME "2018-03-12 15:58:40"
+#define MARS_TAG "samplelibs"
 
 #endif

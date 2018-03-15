3d95e278e385f9d8dc8b48c8585a9a0757065874
gairui <gairui@mogujie.com>
2018-03-15 18:11:18 +0800

    Commented shared_state_base https://svn.boost.org/trac10/ticket/10558#no3

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "c7ae437"
+#define MARS_REVISION "66a60c2"
 #define MARS_PATH "feature/mwcs_iOS_version_of_mars"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2018-03-13 11:16:45"
-#define MARS_TAG "mwcs_[2018-03-13_11.10]@git-c7ae437@gairui"
+#define MARS_BUILD_TIME "2018-03-15 15:56:50"
+#define MARS_TAG "samplelibs"
 
 #endif

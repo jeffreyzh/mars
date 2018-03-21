887dfa7b62ba04d14a3d54aa1505e0ce52a8eff9
gairui <gairui@mogujie.com>
2018-03-21 16:10:04 +0800

    为 mlog 打包

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "66c422e"
+#define MARS_REVISION "a923a9a"
 #define MARS_PATH "feature/mars_v121_with_mwcs"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2018-03-21 15:23:52"
-#define MARS_TAG "samplelibs"
+#define MARS_BUILD_TIME "2018-03-21 15:45:47"
+#define MARS_TAG "log_[2018-03-21_15.45]@git-a923a9a@gairui"
 
 #endif
---
+++ mars/libraries/log_[2018-03-21_15.45]@git-a923a9a@gairui/mars-open-iphone/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "a923a9a"
+#define MARS_PATH "feature/mars_v121_with_mwcs"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2018-03-21 15:45:47"
+#define MARS_TAG "log_[2018-03-21_15.45]@git-a923a9a@gairui"
+
+#endif

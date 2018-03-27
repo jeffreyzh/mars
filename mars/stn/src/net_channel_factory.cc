f77d9d67459ad461ccd43a81ff59a2a143c251d2
gairui <gairui@mogujie.com>
2018-03-27 22:34:20 +0800

    注入的方式修改 longlink

--- mars/stn/src/net_channel_factory.cc
+++ mars/stn/src/net_channel_factory.cc
@@ -25,15 +25,10 @@
 #include "longlink.h"
 #include "shortlink.h"
 
-
-//TODO
-#ifdef MWCSLongLink
-#include "client-mars/mwcs_long_link.h"
-#endif
-
 namespace mars {
 namespace stn {
 
+    
 namespace ShortLinkChannelFactory {
 
     
@@ -53,14 +48,19 @@ void (*Destory)(ShortLinkInterface* _short_link_channel)
 
 namespace LongLinkChannelFactory {
 
+static LongLinkChannelCreator long_link_channel_creater = NULL;
+void (*SetCustomLongLinkChannel)(LongLinkChannelCreator _long_link_channel_creater)
+=[](LongLinkChannelCreator _long_link_channel_creater){
+  long_link_channel_creater = _long_link_channel_creater;
+};
+
 LongLink* (*Create)(const mq::MessageQueue_t& _messagequeueid, NetSource& _netsource)
 = [](const mq::MessageQueue_t& _messagequeueid, NetSource& _netsource) -> LongLink* {
-#ifdef MWCSLongLink
-  return new MWCSLongLink(_messagequeueid, _netsource);
-#else
-  return new LongLink(_messagequeueid, _netsource);
-#endif
 
+  if(long_link_channel_creater!=NULL){
+    return long_link_channel_creater(_messagequeueid,_netsource);
+  }
+  return new LongLink(_messagequeueid, _netsource);
 };
 
 void (*Destory)(LongLink* _long_link_channel)
@@ -68,6 +68,7 @@ void (*Destory)(LongLink* _long_link_channel)
 	delete _long_link_channel;
 	_long_link_channel = NULL;
 };
+
 
 }
 }
